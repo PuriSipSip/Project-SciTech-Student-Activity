ac17c4fa8744b9968a1cac09216c2d43e5f9ec04
PuriSipSip <Dettanawiwat@gmail.com>
2024-05-06 18:20:11 +0700

    first commit

---
+++ src/test/java/com/sp343/projecta/ProjectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp343.projecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
