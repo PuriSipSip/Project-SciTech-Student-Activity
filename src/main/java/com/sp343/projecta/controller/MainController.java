4423f47ac878ccd76691d1e16f6e98be657d3bac
Puttipong2566 <bookkeehohojoko@hotmail.com>
2024-05-06 19:57:25 +0700

    first Data

--- src/main/java/com/sp343/projecta/controller/MainController.java
+++ src/main/java/com/sp343/projecta/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -16,16 +17,17 @@ public class MainController {
     @Autowired
     private ActivityRepository activityRepo;
 
-    @GetMapping("/")
-    String getAllActivities(Model model) {
+    @GetMapping("/activity")
+    public ModelAndView getAllActivities(Model model) {
+        ModelAndView modelAndView = new ModelAndView("index");
         List<Activity> activities = activityRepo.findAll();
-        model.addAttribute("all_activities", activities);
-        return "activities";
-    }
 
-    @GetMapping("/1")
-    public List <Activity > Test(){
-        return activityRepo.findAll();
+        // Check if activities is not null before adding it to the model
+        if (activities != null) {
+            model.addAttribute("all_activities", activities);
+        }
+
+        return modelAndView;
     }
 
 }
